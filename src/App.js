d1faff9c1db80035963fc829287e1151b2c26bf2
DcWind <danielekwuazi@gmail.com>
2017-04-20 23:45:55 +0700

    app state

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     return (
       <div className="App">
         My App
-        <Projects />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
