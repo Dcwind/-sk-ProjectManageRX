fbd8ca78de1942ddef99451e18ee64320375dc77
DcWind <danielekwuazi@gmail.com>
2017-04-20 17:30:55 +0700

    constructor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import Projects from './Components/Projects';
 
 class App extends Component {
+  Constructor(){
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Clinic Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Coffee Shop Logo',
+          category: 'Logo Design'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
