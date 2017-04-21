c7fc7917eb0d51c20d239889a78ef6fe105f7060
DcWind <danielekwuazi@gmail.com>
2017-04-21 23:33:10 +0700

    mapped through project array

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
   render() {
-    console.log(this.props);
+    let projectItem;
+    if(this.props.projects){
+      projectItem = this.props.projects.map(projects => {
+        console.log(projects);
+      });
+    }
     return (
       <div className="Projects">
         My Projects
