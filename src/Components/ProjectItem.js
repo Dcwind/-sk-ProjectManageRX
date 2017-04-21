527a848afa64977be32a041b7188993ca09049b3
DcWind <danielekwuazi@gmail.com>
2017-04-21 23:03:39 +0700

    boiler plate

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 
 class ProjectItem extends Component {
   render() {
-    console.log(this.props);
+    
     return (
       <li className="Projects">
-        My Projects
+        
       </li>
     );
   }
