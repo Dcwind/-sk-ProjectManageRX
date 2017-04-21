f77b3e20cf70fea7b3f6b8a5a0c5b364440f475e
DcWind <danielekwuazi@gmail.com>
2017-04-21 22:37:45 +0700

    props passed

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
   render() {
-    console.log(this.props.projects);
+    console.log(this.props);
     return (
       <div className="Projects">
         My Projects
