5ca14ca9102cbd4c18c38d976ad1ec91e9fbe0d7
DcWind <danielekwuazi@gmail.com>
2017-04-25 21:25:40 +0700

    used life cycle method mount

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: [
-        {
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Clinic Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Coffee Shop Logo',
-          category: 'Logo Design'
-        }
-      ]
+      projects: []
     }
   }
+
+  ComponentWillMount() {
+      this.setState({
+        projects: [ 
+          {
+            title: 'Business Website',
+            category: 'Web Design'
+          },
+          {
+            title: 'Clinic Website',
+            category: 'Web Design'
+          },
+          {
+            title: 'Coffee Shop Logo',
+            category: 'Logo Design'
+          }
+        ]
+      });
+  }
+
   render() {
     return (
       <div className="App">
