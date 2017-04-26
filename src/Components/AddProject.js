270768d5df676cee894bae4cef71877108884427
DcWind <danielekwuazi@gmail.com>
2017-04-26 22:22:45 +0700

    form in progress

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 
 class AddProject extends Component {
+  static defaultProps = {
+    categories: ['Web Design', 'Mobile App Development', 'Logo Design' ]
+  }
   render() {
-  
+  let categoryOptions = this.props.categories.map(category => {
+    return <option value="category" key={category}>{category}</option>
+  });
     return (
       <div >
         <h3>Add Project</h3>
