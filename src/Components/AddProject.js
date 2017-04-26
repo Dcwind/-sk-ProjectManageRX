270e5a66893582d04f9d4a7ac1204f780a6a37d8
DcWind <danielekwuazi@gmail.com>
2017-04-26 15:59:13 +0700

    form in progress

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -7,6 +7,16 @@ class AddProject extends Component {
     return (
       <div >
         <h3>Add Project</h3>
+        <form action="">
+          <div>
+            <label >Title</label> <br />
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label >Category</label> <br />
+            <select name="" ref="category"></select>
+          </div>
+        </form>
       </div>
     );
   }
