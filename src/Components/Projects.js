62e61a23d870ed07c292bb7214451ba877c2fd40
DcWind <danielekwuazi@gmail.com>
2017-04-27 15:22:14 +0700

    Delete fuctionality in progress, passing id for delete up to App component

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
+
+  deleteProject(id){
+    this.props.onDelete(id);
+  }
+
   render() {
   
     let projectItem;
     if(this.props.projects){
       projectItem = this.props.projects.map(project => {
        return (
-         <ProjectItem key={project.title} project={project} />
+         <ProjectItem key={project.title} onDelete={this.deleteProject.bind(this)} project={project} />
        );
       });
     }
